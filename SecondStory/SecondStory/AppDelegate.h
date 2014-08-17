c6ee697f2a0f41aca3d9098d681ead9df0afa221
Jesse Scott <jessecolinscott@gmail.com>
2014-08-17 16:34:30 -0700

    Added Basic Xcode Project

---
+++ SecondStory/SecondStory/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SecondStory
+//
+//  Created by Jesse Scott on 2014-08-17.
+//  Copyright (c) 2014 The Only Animal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
