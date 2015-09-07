6a67f3886957a3e7afda154fc0b62635813d3089
JesseScott <jessecolinscott@gmail.com>
2015-09-07 02:16:41 -0700

    nslayoutconstraints

--- SecondStory/SecondStory/Classes/Delegate/AppDelegate.m
+++ SecondStory/SecondStory/Classes/Delegate/AppDelegate.m
@@ -29,19 +29,15 @@
     /* from http://stackoverflow.com/questions/15268702/implementing-long-running-tasks-in-background-ios */
     
     bgTask = [application beginBackgroundTaskWithExpirationHandler:^{
-        
         // Wait until the pending operations finish
         [operationQueue waitUntilAllOperationsAreFinished];
-        
         [application endBackgroundTask: bgTask];
         bgTask = UIBackgroundTaskInvalid;
     }];
     
     // Start the long-running task and return immediately.
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-        
         // Do the work associated with the task, preferably in chunks.
-        
         [application endBackgroundTask:bgTask];
         bgTask = UIBackgroundTaskInvalid;
     });
